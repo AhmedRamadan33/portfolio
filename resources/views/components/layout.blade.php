3419e25308f02f00c8aad99daa335809de2741a1
AhmedRamadan33 <engabdelaaty3210@gmail.com>
2026-09-07 01:07:20 +0300

    add sitemap

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="HNDDrb5cQoensf_rR5XK7xu2vfQ7wZ0mLflPLRLvyR0" />
 
     @php
         $seoName = $profile->name ?? config('app.name');
